8378e1047f9c6fb9d9a0719222860267b7540d8b
Ariel <sonyt605@gmail.com>
2024-09-26 17:55:26 +0800

    add section Yummy

--- js/swiper.js
+++ js/swiper.js
@@ -3,8 +3,30 @@ import 'swiper/css/bundle';
 
 //首頁
 const swiper = new Swiper(".mySwiper", {
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+  spaceBetween: 24,
+  slidesPerView: 1,
+  slidesPerGroup: 1,
+  grid: {
+    rows: 3,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev"
+  },
+  breakpoints: {
+    992: {      
+      grid: {
+        rows: 1,
+      },
+      slidesPerView: 2,
+      spaceBetween: 48,
     },
-  });
+    1200: {      
+      grid: {
+        rows: 1,
+      },
+      slidesPerView: 3,
+      spaceBetween: 24,
+    }
+  }
+});
